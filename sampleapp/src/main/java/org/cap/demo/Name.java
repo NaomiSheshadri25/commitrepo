7886449e50276c3a390108461b30b3b3084d5843
NaomiSeshadri25 <NaomiSeshadri25@gmail.com>
2021-09-20 11:17:08 +0530

    # WARNING: head commit changed in the meantime
    Merge branch 'master' into ReportFeature

--- sampleapp/src/main/java/org/cap/demo/Name.java
+++ sampleapp/src/main/java/org/cap/demo/Name.java
@@ -4,6 +4,14 @@ public class Name {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello from sts");
+		show();// TODO Auto-generated method stub
+
+
+
+	}
+	public static void show() {
+	System.out.println("Show Method here!");
+	}
 		}
 
-}
+
